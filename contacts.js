922981b56223a50c67fa31d8213ba7e018b122f0
Sara <sarabretes@me.com>
2020-04-11 20:59:12 +0100

    debug inline validation

--- contacts.js
+++ contacts.js
@@ -1,55 +1,66 @@
-function formValidation() {
+function fullNameValidation() {
   let regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
   let name = document.getElementById("fullName").value;
   let errorName = document.getElementById("errorName");
-  let regEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2,})$/;
-  let errorEmail = document.getElementById("errorEmail");
-  let email = document.getElementById("email").value;
-  let regPhoneNumber = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
-  let errorPhone = document.getElementById("errorPhone");
-  let phoneNumber = document.getElementById("phoneNumber").value;
-  let messageText = document.getElementById("messageText").value;
-  let errorTextMessage = document.getElementById("errorTextField");
+  let fullNameIsValid = !regName.test(name);
 
-  if (!regName.test(name)) {
+  if (fullNameIsValid) {
     errorName.style.display = "block";
   } else {
     errorName.style.display = "none";
   }
+  return fullNameIsValid;
+}
 
-  if (!regEmail.test(email)) {
+function emailValidation() {
+  let regEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2,})$/;
+  let errorEmail = document.getElementById("errorEmail");
+  let email = document.getElementById("email").value;
+  let emailIsValid = !regEmail.test(email);
+
+  if (emailIsValid) {
     errorEmail.style.display = "block";
   } else {
     errorEmail.style.display = "none";
   }
+  return emailIsValid;
+}
 
-  if (!regPhoneNumber.test(phoneNumber)) {
+function phoneValidation() {
+  let regPhoneNumber = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
+  let errorPhone = document.getElementById("errorPhone");
+  let phoneNumber = document.getElementById("phoneNumber").value;
+  let phoneNumberIsValid = !regPhoneNumber.test(phoneNumber);
+  if (phoneNumberIsValid) {
     errorPhone.style.display = "block";
   } else {
     errorPhone.style.display = "none";
   }
+  return phoneNumberIsValid;
+}
 
-  if (messageText.length < 10) {
+function messageValidation() {
+  let messageText = document.getElementById("messageText").value;
+  let errorTextMessage = document.getElementById("errorTextField");
+  let messageIsValid = messageText.length < 10;
+
+  if (messageIsValid) {
     errorTextMessage.style.display = "block";
   } else {
     errorTextMessage.style.display = "none";
   }
+  return messageIsValid;
 }
 
-// function handle_form_submission() {
-//   var e = document.getElementById("email"),
-//     d = document.getElementById("error"),
-//     t = document.getElementById("errorName"),
-//     s = document.getElementById("name"),
-//     l = document.getElementById("phone"),
-//     n = document.getElementById("errorPhone");
-//   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2,})$/.test(e.value)
-//     ? (d.style.display = "none")
-//     : (d.style.display = "block"),
-//     "" !== s.value ? (t.style.display = "none") : (t.style.display = "block"),
-//     /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/.test(
-//       l.value
-//     )
-//       ? (n.style.display = "none")
-//       : (n.style.display = "block");
-// }
+function fullFormValidation() {
+  if (
+    fullNameValidation() &&
+    emailValidation() &&
+    phoneValidation() &&
+    messageValidation()
+  ) {
+    console.log("form Submited");
+  } else {
+    console.log("there was a problem with your form");
+  }
+}
