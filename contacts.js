b4b9a095a615450ddc28faa8d34ed33d7259054b
Sara <sarabretes@me.com>
2020-04-11 21:07:34 +0100

    debug

--- contacts.js
+++ contacts.js
@@ -2,12 +2,12 @@ function fullNameValidation() {
   let regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
   let name = document.getElementById("fullName").value;
   let errorName = document.getElementById("errorName");
-  let fullNameIsValid = !regName.test(name);
+  let fullNameIsValid = regName.test(name);
 
   if (fullNameIsValid) {
-    errorName.style.display = "block";
-  } else {
     errorName.style.display = "none";
+  } else {
+    errorName.style.display = "block";
   }
   return fullNameIsValid;
 }
@@ -16,12 +16,12 @@ function emailValidation() {
   let regEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2,})$/;
   let errorEmail = document.getElementById("errorEmail");
   let email = document.getElementById("email").value;
-  let emailIsValid = !regEmail.test(email);
+  let emailIsValid = regEmail.test(email);
 
   if (emailIsValid) {
-    errorEmail.style.display = "block";
-  } else {
     errorEmail.style.display = "none";
+  } else {
+    errorEmail.style.display = "block";
   }
   return emailIsValid;
 }
@@ -30,11 +30,11 @@ function phoneValidation() {
   let regPhoneNumber = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
   let errorPhone = document.getElementById("errorPhone");
   let phoneNumber = document.getElementById("phoneNumber").value;
-  let phoneNumberIsValid = !regPhoneNumber.test(phoneNumber);
+  let phoneNumberIsValid = regPhoneNumber.test(phoneNumber);
   if (phoneNumberIsValid) {
-    errorPhone.style.display = "block";
-  } else {
     errorPhone.style.display = "none";
+  } else {
+    errorPhone.style.display = "block";
   }
   return phoneNumberIsValid;
 }
@@ -45,9 +45,9 @@ function messageValidation() {
   let messageIsValid = messageText.length < 10;
 
   if (messageIsValid) {
-    errorTextMessage.style.display = "block";
-  } else {
     errorTextMessage.style.display = "none";
+  } else {
+    errorTextMessage.style.display = "block";
   }
   return messageIsValid;
 }
