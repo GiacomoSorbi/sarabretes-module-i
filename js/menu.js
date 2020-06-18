77ce963d800aa3596b88cc8a0b46ac759d62b920
Sara Bretes <sarabretes@me.com>
2020-06-18 20:11:07 +0100

    lazy loading images

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,6 @@
 function underlineMenu() {
   const path = window.location.pathname.split("/");
-  console.log(path);
   const page = path[path.length - 1];
-  console.log(page);
   const menuItem = document.querySelector('[data-page="' + page + '"]');
-  console.log(menuItem);
   menuItem.classList.add("currentPage");
 }
